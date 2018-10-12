99f2d434231100f4a60fe19a708b32995092f774
ulas <udegertekin@gmail.com>
2018-10-11 20:35:06 -0500

    shuffling the images function added

--- src/App.js
+++ src/App.js
@@ -7,34 +7,41 @@ import "./App.css";
 class App extends Component {
   state = {
     images,
-    score:0,
-    topscore:0,
-    id:[],
+    score: 0,
+    topscore: 0,
+    id: [],
   };
-  
+
 
   scoreGif = event => {
-   const imageId= event.target.getAttribute("data-id");
-   console.log(imageId);
-   if (this.state.id.includes(imageId)) {
-     console.log("gameover");
-     return;
-   }
-   this.setState({score: this.state.score +1});
-   
-   let tmp= this.state.id;
-   tmp= tmp.push(imageId);
-   this.setState({id: tmp});
-  // console.log(tmp);
-   
+    const imageId = event.target.getAttribute("data-id");
+    //  console.log(imageId);
+    if (this.state.id.includes(imageId)) {
+
+      return this.setState({ score: 0 });
+    }
+    this.setState({ score: this.state.score + 1 });
+    if (this.state.score < this.state.topscore) {
+      console.log("good");
+    }
+    for (let i = images.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [images[i], images[j]] = [images[j], images[i]];
+    }
+
+    //  let tmp= this.state.id;
+    //  tmp= tmp.push(imageId);
+    //  this.setState({id: tmp});
+    // console.log(tmp);
+
 
   };
 
   render() {
     return (
       <div>
-      <Navbar score= {this.state.score}/>
-      
+        <Navbar score={this.state.score} topscore={this.state.topscore} />
+
         {
           this.state.images.map(image => (
             <GifCard
@@ -46,8 +53,8 @@ class App extends Component {
             />
           ))
         }
-        </div>
-    
+      </div>
+
     );
   }
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,26 +3,26 @@ import "./Navbar.css";
 
 const Navbar = props =>
 
-    
-        <div class="row">
-        
-            <div class="col-sm font-weight-bold text-success" >
-                CLICKY GAME
-    </div>
-    <div class="col-sm font-weight-bold text-success">
-                Do not click the same image
-    </div>
-            <div class="col-sm font-weight-bold text-success">
-                <ul>
-                    Score:{props.score}
-                    
-                      |Top Score:{props.topscore}
-                    
-             </ul>
-            </div>
-            
+
+    <div class="row">
+
+        <div class="col-sm font-weight-bold text-success" >
+            CLICKY GAME
         </div>
-    
+        <div class="col-sm font-weight-bold text-success">
+            Do not click the same image
+
+        </div>
+        <div class="col-sm font-weight-bold text-success">
+            <ul>
+                Score: {props.score} |  Top Score: {props.topscore}
+
+            </ul>
+        </div>
+
+    </div>
+
+
 
 
 
